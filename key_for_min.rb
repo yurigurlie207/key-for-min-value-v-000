95d412619d3402e5af1c402494838432fc596de2
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 20:11:15 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,14 @@
 def key_for_min_value(name_hash)
   count = 1
   lowest = 0
-  
+
   name_hash.collect do |name, value|
     if count  == 1 then
       lowest = value
+    elsif value < lowest then
+      lowest = value
     end
+      
   end
 
 
