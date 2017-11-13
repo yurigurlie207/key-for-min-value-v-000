9d020305654a1eaa9bfb7406cfeff2229b15d4a0
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-13 20:12:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest = 0
   name = ""
 
-  name_hash.collect do |name, value|
+  name_hash.each do |name, value|
     if count  == 1 then
       lowest = value
       name = value
